66d486593833fb2fcd6f84beb44d6cc2c05442bf
Michael Krotscheck <krotscheck@gmail.com>
2017-06-07 19:43:55 -0700

    Switch to use protractor and karma directly, instead of ng
    This is done for demo purposes, as it's truer to a flexible tooling
    experience.

--- karma.conf.js
+++ karma.conf.js
@@ -12,11 +12,11 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
@@ -26,7 +26,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
     singleRun: false
   });
